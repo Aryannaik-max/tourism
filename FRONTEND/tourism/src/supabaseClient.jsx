9c300a298b4c4e082825c77672ae9c08e26bd309
Aryannaik-max <naikaryan949@gmail.com>
2025-09-01 01:35:58 +0530

    solved import syntax errors

--- FRONTEND/tourism/src/supabaseClient.jsx
+++ FRONTEND/tourism/src/supabaseClient.jsx
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase.js');
 
-const supabaseUrl = meta.env.VITE_SUPABASE_URL;
-const supabaseKey = meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
