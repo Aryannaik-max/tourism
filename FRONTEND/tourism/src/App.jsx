b0820dc3d2fa47e15d46465e893a9a40a88dc1d7
StephanosNikitis <theshaurya035@gmail.com>
2025-09-08 18:16:03 +0530

    Another nav css fix

--- FRONTEND/tourism/src/App.jsx
+++ FRONTEND/tourism/src/App.jsx
@@ -10,6 +10,7 @@ import Policy from './pages/Policy'
 import Complain from './pages/Complain'
 import ScrollToTop from './ScrollToTop'
 import TransportServices from './pages/TransportServices'
+// import { AuthProvider } from './context/AuthContext'
 
 function App() {
 
@@ -17,16 +18,18 @@ function App() {
     <>
       <Navbar />
       <ScrollToTop />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/culture' element={<Culture />} />
-        <Route path='/tourism' element={<Tourism />} />
-        <Route path='/gallery' element={<Gallery />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/policy' element={<Policy />} />
-        <Route path='/complain' element={<Complain />} />
-        <Route path='/transport' element={<TransportServices />} />
-      </Routes>
+      {/* <AuthProvider> */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/culture' element={<Culture />} />
+          <Route path='/tourism' element={<Tourism />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/policy' element={<Policy />} />
+          <Route path='/complain' element={<Complain />} />
+          <Route path='/transport' element={<TransportServices />} />
+        </Routes>
+      {/* </AuthProvider> */}
       <Footer />
     </>
   )
